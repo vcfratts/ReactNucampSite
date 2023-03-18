88f9d5ef1715816a2489beb1541804d184144426
Victoria <vcfrattarola@gmail.com>
2023-03-18 12:39:13 -0400

    post week 1 react workshop

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-
+import Header from "./components/Header";
+import Footer from './components/Footer';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
 
+import CampSitesList from './features/campsites/CampsitesList';
+import { CAMPSITES } from './app/shared/CAMPSITES';
+
 function App() {
-  return (
-    <div className="App">
-    hello nucamp!
-</div>
-  );
-}
+    return (
+        <div className='App'>
+            <Header/>
+            <CampSitesList/>
+            <Footer/>
+        </div>
+    );
+};
 
 export default App;
