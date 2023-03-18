f166264e70eb0820c8669e1c4897eeb3b5244542
Victoria <vcfrattarola@gmail.com>
2023-03-18 09:10:14 -0400

    initial commit

---
+++ functional-foundations/02-mapping-messages/sweetMessages.js
@@ -0,0 +1,23 @@
+const messages = [
+    'how are you doing',
+    'what are you up to',
+    'would you like to get a bite later'
+];
+
+// const sweetMessages = [
+//     'how are you doing, sweetie?',
+//     'what are you up to, sweetie?',
+//     'would you like to get a bite later, sweetie?',
+// ];
+// const sweetMessage = `${messages{0}}, sweetie?`;
+
+/* const sweetMessages=[];
+
+ for(let i=0; i<messages.length; i++) {
+    const newMessage = `${messages[i]}, sweetie?`;
+    sweetMessages.push(newMessage);
+}
+console.log(sweetMessages); */
+
+const sweetMessages=messages.map(message=>`${message}, sweetie?`);
+console.log(sweetMessages);
